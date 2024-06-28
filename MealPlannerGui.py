f7e75270446c58ecd313e62a45843d38809d6a54
Arshia <arshi829495@gmail.com>
2024-06-28 20:40:22 +0330

    created-foods-page

--- MealPlannerGui.py
+++ MealPlannerGui.py
@@ -9,6 +9,8 @@ class FirstWindow:
         self.root = ctk.CTk()
         # this date used in set_meals function and in nxt func inside of it
         self.date = dt.datetime.now() + dt.timedelta(days=1)
+        # this variable is useful in showing foods
+        self.current_food = ''
         self.Mp1 = Mp.MealPlanner()
         self.root.title("Meal Planner")
         self.root.geometry("800x600")
@@ -37,6 +39,9 @@ class FirstWindow:
             setMealsbtn = ctk.CTkButton(master=menubar_frame, text='Set meals', bg_color='black',
                                         fg_color='black', command=lambda: self.switch_pages(self.set_meals))
             setMealsbtn.pack(fill='x', pady=5)
+            foods_btn = ctk.CTkButton(master=menubar_frame, text='Foods', bg_color='black',
+                                      fg_color='black', command=lambda: self.switch_pages(self.foods_page))
+            foods_btn.pack(fill='x', pady=5)
 
         menu_buttons_frame = ctk.CTkFrame(master=self.root, fg_color='black', bg_color='black')
         menu_buttons_frame.pack(side='top', fill='x')
@@ -197,8 +202,42 @@ class FirstWindow:
         next_day_btn = ctk.CTkButton(master=buttons_frame, text='Next', command=nxt)
         next_day_btn.pack(side='left')
 
+    def foods_page(self):
+        def show_food(food_name):
+            self.current_food = food_name
+            self.switch_pages(self.foods_details)
+
+        all_foods = self.Mp1.show_breakfasts() + self.Mp1.show_lunch_and_dinner()
+        foods_page_main = ctk.CTkFrame(master=self.main_frame, fg_color='#1A1A24', bg_color='#1A1A24')
+        foods_page_main.pack(fill='both', expand=True)
+
+        foods_title_frame = ctk.CTkFrame(master=foods_page_main, height=100, fg_color='#1A1A24', bg_color='#1A1A24')
+        foods_title_frame.pack(side='top', fill='x', pady=5)
+        food_page_title = ctk.CTkLabel(master=foods_title_frame, text='Your Foods & Recipies',
+                                       font=('Times', 25, 'bold'), fg_color='#1A1A24', bg_color='#1A1A24')
+        food_page_title.pack(side='top', fill='both')
+
+        foods_names_frame = ctk.CTkScrollableFrame(master=foods_page_main, fg_color='white', bg_color='#1A1A24', height=10)
+        foods_names_frame.pack(side='top', fill='x', pady=20)
+        foods_names_frame.columnconfigure(0, weight=1)
+        for i in range(len(all_foods)):
+            foods_names_frame.rowconfigure(i, weight=0)
+        btns_lst = []
+        for i in range(len(all_foods)):
+            btn = ctk.CTkButton(master=foods_names_frame, text=all_foods[i], hover_color='gray', anchor='center',
+                                font=('Arial', 15), bg_color='#1A1A24', fg_color='#1A1A24',
+                                command=lambda i=i: show_food(all_foods[i]), border_width=1, border_color='white')
+            btns_lst.append(btn)
+        for i in range(len(btns_lst)):
+            btns_lst[i].grid(row=i, column=0)
+    def foods_details(self):
+        foods_details_main = ctk.CTkFrame(master=self.main_frame, fg_color='#1A1A24', bg_color='#1A1A24')
+        foods_details_main.pack(fill='both', expand=True)
+        foods_details_label = ctk.CTkLabel(master=foods_details_main, text=self.current_food)
+        foods_details_label.pack()
+
     def switch_pages(self, page):
-        # this code must be applied on every page switchs except set_meals switchs to get back date
+        # this code must be applied on every page switches except set_meals switches to get back date
         if page != self.set_meals:
             self.date = dt.datetime.now() + dt.timedelta(days=1)
         for frame in self.main_frame.winfo_children():
